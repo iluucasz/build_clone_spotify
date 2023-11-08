010426c2af59f69072615e401239d128f09578d9
Hyven <lucasface99@gmail.com>
2023-11-08 13:46:38 -0300

    pastas organizada e componentes adicionados

--- clone_spotify/src/App.tsx
+++ clone_spotify/src/App.tsx
@@ -1,9 +1,18 @@
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
+import { Plans } from "./components/plans";
+import { Proposal } from "./components/proposal";
+import { Values } from "./components/values";
 import "./styles/tailwind.css";
 
 function App() {
   return (
     <>
-    
+      <Header />
+      <Values />
+      <Proposal />
+      <Plans />
+      <Footer />
     </>
   )
 }
